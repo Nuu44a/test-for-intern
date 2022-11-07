0ec0f0cb98c7feb7a4884ce45eae1871203279e7
Alexander Apraksin <72949586+Nuu44a@users.noreply.github.com>
2022-11-07 17:46:28 +0300

    changed sh

--- intern-test.sh
+++ intern-test.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/bash
 
 echo "Let's start our pipeline!"
+echo $#
+if [[ $# -ne 3 ]]; then
+  echo "Number of arguments must be 3: Git-repo, filename and some number"
+  exit 1
+fi
 
 echo "Git repo = ${1}"
 echo "ARG1 = ${2}"
